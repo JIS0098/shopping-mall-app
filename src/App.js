819e4b55c60bf8361d38ce0f82f34a9fe4780996
Jisoo Han <llqrw55@naver.com>
2023-04-14 20:36:47 +0900

    상단바 까지 완성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
+
+
+//1. 전체 상품페이지, 로그인페이지, 상품상세 페이지가 필요하다.
+//2. 전체 상품페이지 -전체 상품을 볼수있다
+//3. 로그인버튼을 누르면 로그인 페이지가 나온다.
+//3. 상품디테일을 눌렀으나 로그인이 안된경우 로그인페이지가 먼저 나온다
+//4. 로그인이 되어있을 경우 상품 디테일페이지를 볼수있다.
+//5. 로그아웃 버튼을 누르면 로그아웃이 된다.
+//5. 로그아웃이 되면 상품 디테일페이지를 볼수없다, 다시 로그인페이지가 보인다.
+//6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
+//7. 상품을 검색할수 있다.
+
 
 function App() {
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/> {/*페이지 변경 시에도 고정을 위해 <Navbar/>를 <Routes> 보다 상단에 적어준다*/}
+      <Routes>
+        <Route path="/" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
     </div>
   );
 }
